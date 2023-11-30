a8a7efd1b8f2b3d4e3a0d222389181fff9f4615f
Grant Warkentin <17gwarkentin@gmail.com>
2023-11-30 03:56:39 -0800

    Created seed data;

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create random projects with random tasks
+3.times do |n|
+  Project.create({
+    :name => "Project #{n+1}",
+    :description => "Random text"
+  })
+  project = Project.find(n+1)
+  5.times do |t|
+    project.tasks.create({
+      :name => "Task #{t+1}",
+      :due_date => "#{rand(2018..2028)}-#{rand(1..12)}-#{rand(1..31)}",
+      :complete => rand(2) == 1
+    })
+  end
+end
+
+#create default user
+User.create({
+  :email => "admin@projects.local",
+  :password => "Pass1234",
+  :password_confirmation => "Pass1234"
+})
